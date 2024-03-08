767775cc86b08b73b2997efe264396a6fc3602cb
omprakash kumar <npoint443@gmail.com>
2024-03-08 19:07:19 +0530

    How to take input from the user

--- TheInput/Input.java
+++ TheInput/Input.java
@@ -2,6 +2,7 @@ package TheInput;
 import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
+        /*taking input from the user like integer or float value */
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the value of x = ");
         int x = sc.nextInt();
