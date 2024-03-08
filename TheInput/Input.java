c8ffa2feb3386484ba27120e8f008d3c5c428eb6
omprakash kumar <npoint443@gmail.com>
2024-03-08 18:58:01 +0530

    How to take input in java

---
+++ TheInput/Input.java
@@ -0,0 +1,13 @@
+package TheInput;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the value of x = ");
+        int x = sc.nextInt();
+        System.out.println("the value of x = "+x);
+        System.out.print("enter the value of y = ");
+        float y = sc.nextFloat();
+        System.out.println("the value of y ="+y);
+    }
+}
