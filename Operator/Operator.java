64ee0e72a7703e1cc91eb5d62d86f6772e0d005e
omprakash kumar <npoint443@gmail.com>
2024-03-11 07:23:54 +0530

    About the increament and decreament operator

---
+++ Operator/Operator.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a= 5;
+        a++; 
+        System.out.println("the value of a ="+a);
+        
+        // to used decreament operator 
+        a--;
+        System.out.println("after the decreament value the value of a= "+a);
+    }
+}
