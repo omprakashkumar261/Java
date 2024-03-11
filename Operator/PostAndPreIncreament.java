7df0108009572b007401dc9cb859dfabe752554d
omprakash kumar <npoint443@gmail.com>
2024-03-11 07:34:13 +0530

    All about the post and pre increament and decreament

---
+++ Operator/PostAndPreIncreament.java
@@ -0,0 +1,28 @@
+package Operator;
+
+public class PostAndPreIncreament {
+    public static void main(String[] args) {
+         // post increamentation 
+      int a =5;
+      //int b = a++;
+     // System.out.println("the value of a = "+a);  
+     // System.out.println("the value of b = "+b);
+
+      // pre-increamentation ...
+     // System.out.println("here ! used to the pre- increamentation ");
+     // b=++a;
+     // System.out.println("the value of a= "+a);
+     // System.out.println("the value of b= "+b);
+
+      // post decreametation 
+     // System.out.println("here! the applying the post decreamentation ");
+      //int b=a--;
+      //System.out.println("the value of a= "+a);
+     // System.out.println("the value of b= "+b);
+
+     // pre-decreamentation 
+     int b=--a;
+     System.out.println("the value of a= "+a);
+     System.out.println("the value of b= "+b);
+    }
+}
