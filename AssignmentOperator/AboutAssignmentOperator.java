51131266367576fa7d065d4c325a24ee34807236
omprakash kumar <npoint443@gmail.com>
2024-03-13 07:22:44 +0530

    Assignment Operator and all its types

---
+++ AssignmentOperator/AboutAssignmentOperator.java
@@ -0,0 +1,21 @@
+package AssignmentOperator;
+
+public class AboutAssignmentOperator {
+    public static void main(String []args)
+    {
+        /*here ! used the Assignment Operator have been covered  */
+        int marks= 86;
+        System.out.println(marks);
+
+        /*Another example of Assignment operator  i.e channing Assignment operator  */
+        int m1, m2, m3, m4;
+        m1=m2=m3=m4=87;
+        System.out.println(m1+" "+m2+" "+m3+" "+m4);
+
+        /*here !used the Assignment operator i.e compound Assignment operator  */
+        int add =12; 
+       // add =add+5;//simple adding the value ...
+        add +=5; //this is the compound operator...
+        System.out.println(add);
+    }
+}
