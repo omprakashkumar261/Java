69222c324ab88e831b330c5ae8702939143539d2
omprakash kumar <npoint443@gmail.com>
2024-03-14 07:30:22 +0530

    Relational Operator in java to check here! all condition

---
+++ Operator/RelationalOperator.java
@@ -0,0 +1,44 @@
+package Operator;
+
+public class RelationalOperator {
+    public static void main(String []args)
+    {
+        int a = 10;
+        int b =10;
+        int c =20;
+        boolean result = a==b;
+      System.out.println(result);
+    
+      // to check here ! a greater than as compare to those value....
+      result =a>b;
+      System.out.println(result);
+      result =a>c;
+      System.out.println(result);
+
+      // to check here! a less than as compare to those value .........
+      result = a<b;
+      System.out.println(result);
+      result = a<c;
+      System.out.println(result);
+
+      // to check here! a grater than equal too as comparetivel those value....
+      result = a>=b;
+      System.out.println(result);
+      result = a>=c; 
+      System.out.println(result);
+
+      // to check here! a less than equal too as comparetively those value .....
+      result = a<=b;
+      System.out.println(result);
+      result = a<=c;
+      System.out.println(result);
+
+      //to check here! a not equal too as comparetively to those data...
+      result= a!=b;
+      System.out.println(result);
+      result =a!=c;
+      System.out.println(result);
+
+
+    }
+}
