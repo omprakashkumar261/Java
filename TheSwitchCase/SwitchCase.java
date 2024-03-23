44bf0507c02c874a6bfd46974ae529a63532e770
Omprakash kumar <npoint443@gmail.com>
2024-03-23 12:10:24 +0530

    About the Switch case

---
+++ TheSwitchCase/SwitchCase.java
@@ -0,0 +1,23 @@
+package TheSwitchCase;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int num=8;
+
+        // apply the switch case to solve in this  problem 
+        switch (num) {
+            case 1:
+                System.out.println("case one ");
+                break;
+            case 2:
+                System.out.println("case two");
+                break;
+            case 3:
+                System.out.println("case three");
+        
+            default:
+                System.out.println("invilid num");
+                break;
+        }
+    }
+}
