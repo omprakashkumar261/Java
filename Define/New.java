9d85ebb1fba7ae19707f7749994ca8adbd2dc793
Omprakash kumar <npoint443@gmail.com>
2024-03-29 15:25:03 +0530

    Deffrence between the print and println

---
+++ Define/New.java
@@ -0,0 +1,15 @@
+package Define;
+
+public class New {
+    public static void main(String[] args) {
+        //use of println in java 
+        /*that print the massage on the consol with create a new line */
+        System.out.println("*");
+        System.out.println("*");
+
+        /*that print the massage on the consol without creating a new line */
+        System.out.print("* ");
+        System.out.print("&");
+    }
+    
+}
