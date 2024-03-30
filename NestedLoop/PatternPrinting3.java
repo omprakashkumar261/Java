7695b121bc7b3ef8ecf42f2312c732f10a0dcff0
Omprakash Kuamr <npoint443@gmail.com>
2024-03-30 15:26:27 +0530

    To print ster the multiple as your choice

--- NestedLoop/PatternPrinting3.java
+++ NestedLoop/PatternPrinting3.java
@@ -2,6 +2,53 @@ package NestedLoop;
 
 public class PatternPrinting3 {
     public static void main(String[] args) {
+        // print the star as a like multiple square as same line 
+        int n=14;// to initialise the no.of star for the multiple square 
+        for(int i=1; i<n; i++)//to create the outer loop for the control outline i.e rows 
+        {
+            for(int j=1; j<n; j++)// to create the inner loop for the control the innerline i.e columns
+            {
+                if (i==1 || j==1 || i==(n-1) ||j==(n-1)) 
+                {
+                  System.out.print("*");  
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+           // System.out.println();
+            System.out.print("     ");
+
+            // to print the second star priting for multiple time
+            for(int j=1; j<n; j++)// to create the inner loop for the control the innerline i.e columns
+            {
+                if (i==1 || j==1 || i==(n-1) ||j==(n-1)) 
+                {
+                  System.out.print("*");  
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            //System.out.println();
+            System.out.print("     ");
+
+            // to print third star printing go for the multiple time
+            for(int j=1; j<n; j++)// to create the inner loop for the control the innerline i.e columns
+            {
+                if (i==1 || j==1 || i==(n-1) ||j==(n-1)) 
+                {
+                  System.out.print("*");  
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
         
     }
 }
