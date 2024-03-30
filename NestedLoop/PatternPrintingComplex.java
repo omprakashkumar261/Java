b9a14420c0919e04a97c1151593862401a07ca56
Omprakash Kumar <npoint443@gmail.com>
2024-03-30 17:00:10 +0530

    Print the star like -in- diognal line

--- NestedLoop/PatternPrintingComplex.java
+++ NestedLoop/PatternPrintingComplex.java
@@ -3,13 +3,13 @@ package NestedLoop;
 public class PatternPrintingComplex {
     public static void main(String [] args){
         // To print star diognal line 
-        
+
         int n=12; // To initialise the no.of start to need the print 
         for(int i=1; i<n; i++)// to create the outer loop for the control the outer line 
         {
             for(int j=1; j<n; j++)// to create the inner loop for the control the inner line 
             {
-                if(i==j)
+                if(i==j)// to check the condition there need the star in form of diognol
                 {
                     System.out.print("*");
                 }
