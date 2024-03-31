86bdd59d824813888f8b4c835e0ca3b787ff8b43
Omprakash Kumar <npoint443@gmail.com>
2024-03-31 11:12:08 +0530

    Create the square inside the rectangular

---
+++ NestedLoop/PatternPrintingCrossSquareInsideTheSquare.java
@@ -0,0 +1,25 @@
+package NestedLoop;
+
+public class PatternPrintingCrossSquareInsideTheSquare {
+    public static void main(String[] args) {
+    // we need to do print like printed the star cross square inside the square 
+    int n=17;// there initialise the no.of star 
+    //to create the outer loop for the control outer line i.e- rows 
+    for(int i=1; i<n; i++)//to create the outer loop to control the outer line
+    {
+        for(int j=1; j<n; j++)// to create the inner loop to control the inner line 
+        {
+            if(i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==(n-1)+(n-1)/2  || i==1 || j==1 || i==(n-1) || j==(n-1))
+            {
+                System.out.print("*");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+        
+    }
+}
