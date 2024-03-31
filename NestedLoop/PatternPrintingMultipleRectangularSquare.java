65523ab0bc6fb1dc29c52c9f4a36f58cc2fcba8f
Omprakash Kumar <npoint443@gmail.com>
2024-03-31 11:57:17 +0530

    Create the multiple rectangular inside the rectangular

---
+++ NestedLoop/PatternPrintingMultipleRectangularSquare.java
@@ -0,0 +1,22 @@
+package NestedLoop;
+
+public class PatternPrintingMultipleRectangularSquare {
+    public static void main(String[] args) {
+        int n=17;// initialise the no.of star in the rectangular 
+        for(int i=1; i<n; i++)// to create the outer loop for control the outer line i.e- rows 
+        {
+            for(int j=1; j<n; j++)//to create the inner loop go for control the inner line i.e- columns
+            {
+                if(i==1 || j==1 || i==(n-1) || j==(n-1) || i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==(n-1)+(n-1)/2 || i==j || i+j==(n-1))// to check the condition there needed the star
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }//end the inner loop 
+            System.out.println();
+        }// end the outer loop that means end the outer line 
+    }
+}
