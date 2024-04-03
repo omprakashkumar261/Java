de9934a866c4be3b01b16a1bbb68ace69d6668a8
Omprakash Kumar <npoint443@gmail.com>
2024-04-03 16:35:31 +0530

    Parametterized method in java with example

---
+++ Method2/Method_Creation2.java
@@ -0,0 +1,17 @@
+package Method2;
+
+public class Method_Creation2 {
+    // create the method to get the result of addition by parametterized way 
+    public static void Addition(int a, int b)
+    {
+        int res=a+b;
+        System.out.println(res);
+    }
+    public static void main(String [] args){
+        System.out.println("Hello OP Sharma !");
+        int a=12;
+        int b=34;
+        Addition(10, 15);
+
+    }
+}
