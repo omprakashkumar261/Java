38255e0273e7ae4a2673b0993b9abb02d3adca9b
Omprakash Kumar <npoint443@gmail.com>
2024-04-07 11:24:51 +0530

    to create the explicite typecasting

---
+++ Method3Practice/Explicite_Typecasting.java
@@ -0,0 +1,16 @@
+package Method3Practice;
+
+public class Explicite_Typecasting {
+    public static void main(String [] args){
+        System.out.println("Hello Op Sharma ! welcome....");
+        explicite_Typecasting();
+    }//end the main method 
+    // to create the explicite typecasting method to get result 
+    public static void explicite_Typecasting()
+    {
+        double a=15;
+        int b;
+        b=(int)a;
+        System.out.println(b);
+    }
+}
