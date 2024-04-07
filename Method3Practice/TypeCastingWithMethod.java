f95b7a57a5b64ab83e60b61c85653e6ef13537ce
Omprakash Kumar <npoint443@gmail.com>
2024-04-07 11:13:01 +0530

    to create the implicite typecasting

---
+++ Method3Practice/TypeCastingWithMethod.java
@@ -0,0 +1,17 @@
+package Method3Practice;
+
+public class TypeCastingWithMethod {
+    public static void main(String [] args){
+        System.out.println("Hello OP Sharma ! welcome...");
+        type_Casting();
+    }// here ! end the main method 
+    // to create the method to get the result for typecasting as your choice 
+    public static void type_Casting()
+    {
+        // Here ! crete the implicite typecasting to get result on the base your value...
+        int a=34;
+        double b;
+        b=a;
+        System.out.println(b);
+    }
+}
