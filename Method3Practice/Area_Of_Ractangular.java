2d91b008b6e0ada304dd316272aa87a5cc98686b
Omprakash Kumar <npoint443@gmial.com>
2024-04-07 11:45:43 +0530

    to create the method to get find the area of ractangular

---
+++ Method3Practice/Area_Of_Ractangular.java
@@ -0,0 +1,17 @@
+package Method3Practice;
+
+public class Area_Of_Ractangular {
+    public static void main(String[] args) {
+        System.out.println("Hello Op sharma ! welcome...");
+        ractangular_Area(); 
+    }
+    // to create the method to find the area of ractangular
+    public static void ractangular_Area()
+    {
+        int l=4;
+        int b=20;
+        // find the area of ractangular 
+        int total_Area=l*b;
+        System.out.println(total_Area);
+    }
+}
