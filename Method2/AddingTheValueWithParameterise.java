33d6af587401cefd7698dda44fcd557eb92a3cd6
Omprakash Kumar <npoint443@gmail.com>
2024-04-10 21:59:28 +0530

    Adding the two value by parameterise method

---
+++ Method2/AddingTheValueWithParameterise.java
@@ -0,0 +1,16 @@
+package Method2;
+
+public class AddingTheValueWithParameterise {
+    public static void main(String [] args){
+        System.out.println("Hello Op sharma ! welcome...");
+        int a=87;
+        int b=45;
+        Adding(a, b);
+    }//end the main method.....
+    // create the method  to get result to get excutes the set of instructions.....
+    public static void Adding(int a, int b)
+    {
+        int res=a+b;
+        System.out.println(res);
+    }//?end the Adding method....
+}
