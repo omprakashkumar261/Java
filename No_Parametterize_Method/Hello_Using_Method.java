d9ba3224f5a4e2c9cff39f375f12f531e9c29c61
Omprakash Kumar <npoint443@gmail.com>
2024-05-23 09:18:47 +0530

    Hello java print karo using the non parametterize method

---
+++ No_Parametterize_Method/Hello_Using_Method.java
@@ -0,0 +1,15 @@
+package No_Parametterize_Method;
+
+public class Hello_Using_Method {
+    public static void main(String[] args) {
+        // simple display the any information as your like -
+        System.out.println("Hey! java welcome to in this class for today new day ");
+        introduction();// now here! calling the method to get the result for printing the Hello ! java....
+    }
+    //print the Hello java using the created method from userside....
+    // create the method for the get print the Hello world...
+    public static void introduction() {
+        // print the Hello ! java...
+        System.out.println("Hello! java....");
+    }
+}
